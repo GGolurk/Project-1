4e5d6fe036d9a640e66d8b8816f2fc0bd6f577ab
NMerk <164964437+GGolurk@users.noreply.github.com>
2025-01-10 15:35:33 +0000

    Updated hello.py to ask for user's name.

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-# This program prints "Hello, world!" to the user.
+# This program asks a user for their name, and uses it to greet them.
 
-print("Hello, world!")
+# Asking a user for their name.
+name = input("Enter your name. ")
+# Printing a greeting with their name.
+print("Hello, " + name + "!")
